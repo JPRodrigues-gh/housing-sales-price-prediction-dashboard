a85538c4c0eac98ba648c12e9fb7db7efdc8ca7c
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-11-22 09:14:29 +0000

    Add page_predict_sale_price

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from app_pages.multi_page import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_sale_price_study import page_sale_price_study_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-# from app_pages.page_predict_sale_price import page_predict_sale_price_body
+from app_pages.page_predict_sale_price import page_predict_sale_price_body
 
 app = MultiPage(app_name= "House Price Predictor") # Create an instance of the app 
 
@@ -19,6 +19,6 @@ app = MultiPage(app_name= "House Price Predictor") # Create an instance of the a
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("House Sale Price Study", page_sale_price_study_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-# app.add_page("ML: House Sale Price Prediction", page_predict_sale_price_body)
+app.add_page("ML: House Sale Price Prediction", page_predict_sale_price_body)
 
 app.run() # Run the  app
---
+++ app_pages/page_predict_sale_price.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.file_management import load_clean_data, load_pkl_file
+from src.eval_pipeline_perf import regression_performance
+from src.eval_pipeline_perf import regression_evaluation_plots
+
+
+def page_predict_sale_price_body():
+
+    # load sale price pipeline files
+    ver = 'v1'
+    path = "outputs/ml_pipeline/predict_saleprice/v1"
+    sale_price_pipe = load_pkl_file(f"{path}/best_regressor_pipeline.pkl")
+    sale_price_feat_importance = plt.imread(f"{path}/feature_importance.png")
+    X_train = pd.read_csv(f"{path}/X_train.csv")
+    X_test = pd.read_csv(f"{path}/X_test.csv")
+    y_train = pd.read_csv(f"{path}/y_train.csv")
+    y_test = pd.read_csv(f"{path}/y_test.csv")
+
+    st.write("### ML Pipeline: Predict House Sale Price")
+    # display pipeline training summary conclusions
+    st.info(
+        f"* A Regressor model was chosen to predict sale price "
+        f"for a given property\n"
+        f"* Both feature selection and PCA produced similar results and met "
+        f"business requirement 1. By a small margin, feature selection "
+        f"performed better. Therefore, the best pipeline to use will be that "
+        f"of feature selection.\n"
+        f"* Feature selection achieved an R2 Score: 0.97 on the train set and "
+        f"an R2 Score: 0.78 on the test set.\n"
+        f"* The client requirement is for an R2 Score of 0.75\n"
+        )
+    st.write("---")
+
+    # show pipeline steps
+    st.write("### ML pipeline to predict sale price")
+    st.write(sale_price_pipe)
+    st.write("---")
+
+    # show best features
+    st.write("### The features the model was trained on and their importance")
+    st.write(X_train.columns.to_list())
+    st.image(sale_price_feat_importance)
+    st.write("---")
+
+    # evaluate pipeline performance
+    st.write("### Evaluate Pipeline Performance")
+    regression_performance(X_train, y_train, X_test, y_test, sale_price_pipe)
+    regr_eval_plots = plt.imread(f"{path}/regression_evaluation_plots.png")
+    st.image(regression_evaluation_plots)
+    st.write("---")
