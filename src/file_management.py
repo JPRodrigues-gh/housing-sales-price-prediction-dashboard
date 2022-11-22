067e643e68d9524cf3b2b84bc111a1ce1610ed56
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-11-22 04:59:14 +0000

    Add function to load clean data file

--- src/file_management.py
+++ src/file_management.py
@@ -10,9 +10,15 @@ import joblib
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_housing_price_data():
-    df = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/house_prices_records.csv")
+    path = "inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/"
+    file = "house_prices_records.csv"
+    df = pd.read_csv(path + file)
     return df
 
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_clean_data():
+    df = pd.read_csv("outputs/datasets/cleaned/clean_house_price_records.csv")
+    return df
 
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
